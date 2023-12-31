f7de8642b308c388d07e4f5cf624ca02488434f3
LugpDev2022 <lugpdev2022@gmail.com>
2023-12-31 13:34:52 -0600

    Conexion a mongodb

--- src/app/api/ping/route.js
+++ src/app/api/ping/route.js
@@ -1,5 +1,7 @@
 import { NextResponse } from 'next/server';
+import { connectDB } from '@/utils/mongoose';
 
 export const GET = () => {
+  connectDB();
   return NextResponse.json({ message: 'hello world!' });
 };
