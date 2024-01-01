5240ed5972a75dd0e809f77ab5220b904833ab1e
LugpDev2022 <lugpdev2022@gmail.com>
2023-12-31 20:37:56 -0600

    Listando tareas

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,22 @@
-const Home = () => {
-  return <div>Home</div>;
+import TaskCard from '@/components/TaskCard';
+import Task from '@/models/Task';
+import { connectDB } from '@/utils/mongoose';
+
+const loadTasks = async () => {
+  connectDB();
+  return await Task.find();
+};
+
+const Home = async () => {
+  const tasks = await loadTasks();
+
+  return (
+    <div className='grid grid-cols-3 gap-2'>
+      {tasks.map((task) => (
+        <TaskCard key={task.id} task={task} />
+      ))}
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/components/TaskCard.jsx
@@ -0,0 +1,10 @@
+const TaskCard = ({ task }) => {
+  return (
+    <div className='bg-gray-800 p-10 text-white rounded-md hover:cursor-pointer hover:bg-gray-900'>
+      <h3>{task.title}</h3>
+      <p>{task.description}</p>
+    </div>
+  );
+};
+
+export default TaskCard;
