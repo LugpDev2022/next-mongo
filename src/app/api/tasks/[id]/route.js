649f6466517cec3898efc235cce29fac89bbfd0f
LugpDev2022 <lugpdev2022@gmail.com>
2023-12-31 20:21:53 -0600

    Manejadores de peticiones http

--- src/app/api/tasks/[id]/route.js
+++ src/app/api/tasks/[id]/route.js
@@ -1,13 +1,42 @@
 import { NextResponse } from 'next/server';
+import { connectDB } from '@/utils/mongoose';
+import Task from '@/models/Task';
 
-export const GET = (req, { params: { id } }) => {
-  return NextResponse.json({ message: `obteniendo tarea ${id}` });
+export const GET = async (req, { params: { id } }) => {
+  try {
+    connectDB();
+    const taskFound = await Task.findById(id);
+
+    if (!taskFound)
+      return NextResponse.json({ message: 'Task not found' }, { status: 404 });
+
+    return NextResponse.json(taskFound);
+  } catch (e) {
+    return NextResponse.json(e.message, { status: 400 });
+  }
 };
 
-export const DELETE = (req, { params: { id } }) => {
-  return NextResponse.json({ message: `eliminando tarea ${id}` });
+export const DELETE = async (req, { params: { id } }) => {
+  try {
+    const deletedTask = await Task.findByIdAndDelete(id);
+
+    if (!deletedTask)
+      return NextResponse.json({ message: 'Task not found' }, { status: 404 });
+
+    return NextResponse.json(deletedTask);
+  } catch (e) {
+    return NextResponse.json(e.message, { status: 400 });
+  }
 };
 
-export const PUT = (req, { params: { id } }) => {
-  return NextResponse.json({ message: `actualizando tarea ${id}` });
+export const PUT = async (req, { params: { id } }) => {
+  try {
+    const data = await req.json();
+
+    const updatedTask = await Task.findByIdAndUpdate(id, data, { new: true });
+
+    return NextResponse.json(updatedTask);
+  } catch (e) {
+    return NextResponse.json(e.message, { status: 400 });
+  }
 };
--- src/app/api/tasks/route.js
+++ src/app/api/tasks/route.js
@@ -1,9 +1,22 @@
 import { NextResponse } from 'next/server';
+import { connectDB } from '@/utils/mongoose';
+import Task from '@/models/Task';
 
-export const GET = () => {
-  return NextResponse.json({ message: 'obteniendo tareas' });
+export const GET = async () => {
+  connectDB();
+  const tasks = await Task.find();
+
+  return NextResponse.json({ tasks });
 };
 
-export const POST = () => {
-  return NextResponse.json({ message: 'creando tareas' });
+export const POST = async (req) => {
+  try {
+    const data = await req.json();
+
+    const savedTask = await new Task(data).save();
+
+    return NextResponse.json(savedTask);
+  } catch (e) {
+    return NextResponse.json(e.message, { status: 400 });
+  }
 };
